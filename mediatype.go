39ac4d2049d0b950cd9a5d1853dc32189fa53602
schmidtw <Weston_Schmidt@alumni.purdue.edu>
2025-04-26 20:51:51 -0700

    feat:Add media types and a way to get them all.

--- mediatype.go
+++ mediatype.go
@@ -6,13 +6,32 @@ package wrphttp
 type mediaType string
 
 const (
-	mtJSON        mediaType = "application/wrp+json"
-	mtMsgpack     mediaType = "application/wrp+msgpack"
-	mtOctetStream mediaType = "application/wrp+octet-stream"
-	mtJSONL       mediaType = "application/wrp+jsonl"
-	mtMsgpackL    mediaType = "application/wrp+msgpackl"
+	MEDIA_TYPE_JSON         = "application/wrp+json"
+	MEDIA_TYPE_MSGPACK      = "application/wrp+msgpack"
+	MEDIA_TYPE_OCTET_STREAM = "application/wrp+octet-stream"
+	MEDIA_TYPE_JSONL        = "application/wrp+jsonl"
+	MEDIA_TYPE_MSGPACKL     = "application/wrp+msgpackl"
+
+	mtJSON        mediaType = MEDIA_TYPE_JSON
+	mtMsgpack     mediaType = MEDIA_TYPE_MSGPACK
+	mtOctetStream mediaType = MEDIA_TYPE_OCTET_STREAM
+	mtJSONL       mediaType = MEDIA_TYPE_JSONL
+	mtMsgpackL    mediaType = MEDIA_TYPE_MSGPACKL
 )
 
 func (mt mediaType) String() string {
 	return string(mt)
+}
+
+// AllMediaTypes returns a list of all the media types supported by the encoder.
+// This allows new formats to be added in the future without breaking existing
+// code.  The list is not guaranteed to be in any particular order.
+func AllMediaTypes() []string {
+	return []string{
+		MEDIA_TYPE_MSGPACKL,
+		MEDIA_TYPE_MSGPACK,
+		MEDIA_TYPE_JSONL,
+		MEDIA_TYPE_JSON,
+		MEDIA_TYPE_OCTET_STREAM,
+	}
 }
---
+++ mediatype_test.go
@@ -0,0 +1,40 @@
+// SPDX-FileCopyrightText: 2025 Comcast Cable Communications Management, LLC
+// SPDX-License-Identifier: Apache-2.0
+
+package wrphttp
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAllMediaTypes(t *testing.T) {
+	tests := []struct {
+		name     string
+		expected []string
+	}{
+		{
+			name: "all media types",
+			expected: []string{
+				MEDIA_TYPE_JSON,
+				MEDIA_TYPE_MSGPACK,
+				MEDIA_TYPE_OCTET_STREAM,
+				MEDIA_TYPE_JSONL,
+				MEDIA_TYPE_MSGPACKL,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := AllMediaTypes()
+
+			sort.Strings(actual)
+			sort.Strings(test.expected)
+
+			assert.ElementsMatch(t, test.expected, actual)
+		})
+	}
+}
